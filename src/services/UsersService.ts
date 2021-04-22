27cd3c8493a5233ae50f8aa1d2afe08aa9408c34
Josué Araújo <josue22595@hotmail.com>
2021-04-22 17:19:28 -0300

    Add websocket implementation and dummy interface to use it

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -30,6 +30,12 @@ class UsersService{
   }
 
 
+  async findByEmail(email: string) {
+    const user = await this.usersRepository.findOne({ email });
+  
+    return user;
+  }
+
 }
 
 export { UsersService };
